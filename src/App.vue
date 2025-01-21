5fab3b97c8c95339b3a65ca21f9168e7c630e63b
KEIVISSON CAMPOS <keivisson.dev@gmail.com>
2025-01-21 11:56:50 -0300

    created the script to select tasks and check quantity

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,89 @@
 <script setup>
+import { reactive } from 'vue';
+
+const estado = reactive({
+    filtro: 'todas',
+    tarefas: [
+        {
+            titulo: 'Estudar ES6',
+            finalizada: true,
+        },
+        {
+            titulo: 'Estudar SASS',
+            finalizada: false,
+        },
+        {
+            titulo: 'Ir para a academia',
+            finalizada: false,
+        },
+    ],
+})
+
+const getTarefasPendentes = () =>{
+    return estado.tarefas.filter(tarefa => !tarefa.finalizada)
+}
+
+const getTarefasFinalizadas = () =>{
+    return estado.tarefas.filter(tarefa => tarefa.finalizada)
+}
+
+const getTarefasFiltradas = () =>{
+    const filtro = estado.filtro;
+    switch (filtro) {
+        case 'pendentes':
+            return getTarefasPendentes()
+            break;
+        case 'finalizadas':
+            return getTarefasFinalizadas()
+            break;
+        default:
+            return estado.tarefas
+            break;
+    }
+}
+
 </script>
 
 <template>
-<div class="container">
-    <header class="bg-light p-5 mb-4 mt-4 rounded-3">
-        <h1>Minhas tarefas</h1>
-        <p>
-            Você possui 7 tarefas pendentes
-        </p>
-    </header>
-    <form>
-        <div class="row">
-            <div class="col">
-                <input type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+    <div class="container">
+        <header class="bg-light p-5 mb-4 mt-4 rounded-3">
+            <h1>Minhas tarefas</h1>
+            <p>
+                Você possui {{getTarefasPendentes().length}} tarefas pendentes
+            </p>
+        </header>
+        <form>
+            <div class="row">
+                <div class="col">
+                    <input type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control" />
+                </div>
+                <div class="col-md-2 mb-1 mt-1 mb-md-0 mt-md-0">
+                    <select @change="e => estado.filtro = e.target.value" class="form-control">
+                        <option value="todas">Todas tarefas</option>
+                        <option value="pendentes">Pendentes</option>
+                        <option value="finalizadas">Finalizadas</option>
+                    </select>
+                </div>
+                <div class="col-md-2">
+                    <button type="submit" class="btn btn-primary">Cadastrar</button>
+                </div>
             </div>
-            <div class="col-md-2 mb-1 mt-1 mb-md-0 mt-md-0">
-                <select class="form-control">
-                    <option value="todas">Todas tarefas</option>
-                    <option value="pendentes">Pendentes</option>
-                    <option value="finalizadas">Finalizadas</option>
-                </select>
-            </div>
-            <div class="col-md-2">
-                <button type="submit" class="btn btn-primary">Cadastrar</button>
-            </div>
-        </div>
-    </form>
-    <ul class="list-group mt-4 bg-danger">
-        <li class="list-group-item">
-            <input type="checkbox">
-            <label class="ms-3" for="">
-                Estudar ES6
-            </label>
-        </li>
-    </ul>
-</div>
+        </form>
+        <ul class="list-group mt-4 bg-danger">
+            <li class="list-group-item" v-for="(tarefa, index) in getTarefasFiltradas()" :key="index">
+                <input type="checkbox" :checked="tarefa.finalizada" :id="tarefa.titulo" />
+                <label :class="{done: tarefa.finalizada}" class="ms-3" :for="tarefa.titulo">
+                    {{ tarefa.titulo }}
+                </label>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
+
+.done{
+    text-decoration: line-through;
+}
 
 </style>
